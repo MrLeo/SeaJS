2a24e0a4bd81141d7ad2e8c571b9a81aead9383b
MrLeo <lxbin6819@vip.qq.com>
2015-06-21 11:49:58 +0800

    update

--- static/js/data/version.js
+++ static/js/data/version.js
@@ -1,5 +1,5 @@
 define({
-	'version' : 'v0.0.1',
+	'version' 	: 'v0.0.1',
 	'powerBy'	: 'Leo',
-	'desc' : 'SeaJS模块化编程学习'
+	'desc' 		: '学习SeaJS模块化编程'
 });
--- static/js/pageController/index.js
+++ static/js/pageController/index.js
@@ -4,12 +4,12 @@ define(function(require,exports,module){
 	var $ = require('jquery'); //-引入JQuery
 	var v = require('data/version');
 
-	var text = "这是通过seajs引用jQuery写入的。</br></hr></br>"
+	var text = "这是通过seajs引用jQuery写入的。</br><hr></br>"
 				+ "版本号：" + v.version + "</br>"
 				+ "作者：" + v.powerBy + "</br>"
 				+ "简介：" + v.desc + "</br>";
 	$('#output').html(text);
 	
 	var alert = require('alert');
-	swal('test alert');
+	swal("版本号：" + v.version + "\n" + "作者：" + v.powerBy + "\n" + "简介：" + v.desc);
 });
