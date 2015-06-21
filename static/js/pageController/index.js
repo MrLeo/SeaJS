f96385286a352e33ea3182dce7dfa6443d7000c4
MrLeo <lxbin6819@vip.qq.com>
2015-06-21 10:29:30 +0800

    完善config
    完善seajs的config信息
    添加sweetalert

--- static/js/pageController/index.js
+++ static/js/pageController/index.js
@@ -1,7 +1,10 @@
 define(function(require,exports,module){
 	console.log("进入:",module.id);
 	
-	var $ = require('jquery');
+	var $ = require('jquery'); //-引入JQuery
 
-	$('#output').text("这是通过seajs引用jQuery写入的。");
+	$('#output').text("这是通过seajs引用jQuery写入的。"); 
+	
+	var alert = require('alert');
+	swal('test alert');
 });
--- static/libs/main.js
+++ static/libs/main.js
@@ -1,41 +1,51 @@
 define(function(require, exports, module) {
-	//seajs配置信息
+	/* ---- seajs配置信息,更多配置信息参照：https://github.com/seajs/seajs/issues/262 ---- */
 	seajs.config({
-		base: '/SeaJS/static/js', //设置根路径
-		alias: { //设置常用模块的别名
-			'jquery': '../../libs/jquery/jquery-2.0.3'
+		/* ---- 请根据Web服务(IIS/Apache...)和项目结构调整base的路径 ---- */
+		base: '/SeaJS/static/js', //--设置根路径
+		paths: { //--设置常用路径的别名
+			'commen'	: 'commen', 					//--业务共通过模块
+			'data'		: 'data', 						//--数据、配置信息
+			'page'		: 'pageController', 			//--页面对应的模块
+			/* ---- 以下是和业务无关的库 ---- */
+			'seajs'		: '../../libs/seajs', 			//--seajs原始库
+			'jquery'	: '../../libs/jquery', 			//--JQuery库
+			'alert'		: '../../libs/sweetalert/dist', //--SweetAlert
+			'tools'		: '../../libs/tools' 			//--工具库
 		},
-		paths: { //设置常用路径
-			'commen': 'commen',//业务共通过模块
-			'data': 'data',//数据、配置信息
-			'page': 'pageController',//页面对应的模块
-			/*以下是和业务无关的库*/
-			'tools' : '../../libs/tools',//工具库
-			'jquery': '../../libs/jquery',//JQuery库
-			'seajs' : '../../libs/seajs'//seajs原始库
-		}
+		alias: { //--设置常用模块的别名
+			'jquery'	: 'jquery/jquery-2.0.3',
+			'alert'		: 'alert/sweetalert.min'
+		},
+		vars: { //--变量配置
+			'locale'	: 'zh-cn'
+		},
+		preload: [ //--预先加载
+			'jquery'
+		],
+		debug: true, //--调试模式
+		charset: 'utf-8' //--文件编码
 	});
 
-	//获取当前页面名，不包含".html"
+	/* ---- 获取当前页面名，不包含".html" ---- */
 	function getCurrentPage() {
 		var urls = location.href.split("/");
 		var page = urls[urls.length - 1];
 		return page.split(".html")[0];
 	}
 
-	//加载页面对应的js
+	/* ---- 加载页面对应的js ---- */
 	exports.load = function(basePath) {
 		if (basePath) {
 			seajs.config({
 				'base': basePath
 			});
 		}
-		console.log("controller", getCurrentPage());
-		require.async('page/' + getCurrentPage());
+		require.async(['page/' + getCurrentPage()], function(page) {
+			//--异步加载多个模块，在加载完成时，执行回调
+		});
 	};
 
-	if (!~module.id.indexOf('?auto')) {
-		console.log("auto load controller");
+	if (!~module.id.indexOf('?1')) //--判断是否自动load（说明：~-1=0）
 		exports.load();
-	}
 });
