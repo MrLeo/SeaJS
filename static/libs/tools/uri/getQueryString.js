0927052cbf4d12dd5d121e914973c5a0942eb8de
MrLeo <lxbin6819@vip.qq.com>
2015-08-19 13:35:52 +0800

    update

--- static/libs/tools/uri/getQueryString.js
+++ static/libs/tools/uri/getQueryString.js
@@ -1,5 +1,9 @@
 define(function(require, exports, module) {
-	module.exports = { // 或者通过 module.exports 提供整个接口
+	module.exports = {
+		/**
+		 * 获取URL参数
+		 * 		getQueryString('参数名');
+		 */
 		'getQueryString': function(name) {
 			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
 			var r = window.location.search.substr(1).match(reg);
