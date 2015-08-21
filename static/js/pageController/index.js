1cabcc0615e4c155245f7343a7ea037d6abf4c82
MrLeo <lxbin6819@vip.qq.com>
2015-08-21 23:09:44 +0800

    update

--- static/js/pageController/index.js
+++ static/js/pageController/index.js
@@ -1,10 +1,11 @@
 define(function (require, exports, module) {
-    console.log("进入:", module.id);
-
-    var $ = require('jquery'); //-引入JQuery
+    var $ = require('jquery');
     var v = require('data/version');
     var d = require('tools/convert/date');
     var m = require('commen/baidu-map');
+    var console = require('console');
+
+    console.log("进入:", module.id);
 
     var text = "这是通过seajs引用jQuery写入的。</br><hr></br>" + "版本号：" + v.version + "</br>" + "作者：" + v.powerBy + "</br>" + "简介：" + v.desc + "</br>";
     $('#output').html(text);
