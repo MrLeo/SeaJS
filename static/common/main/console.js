1d1590a2403fbedbef5db787bfd4effdad28eaa3
MrLeo <lxbin6819@vip.qq.com>
2016-01-28 14:54:54 +0800

    console.log输出添加自定义样式

--- static/common/main/console.js
+++ static/common/main/console.js
@@ -1,10 +1,9 @@
-define(function (require, exports, module) {
-    exports.log = function () {
-        if (window.console && console.log) {
-            console.log('[LEO]',arguments);
-        }
-        else {
-            alert(msg);
-        }
-    }
+define(function(require, exports, module) {
+	exports.log = function() {
+		if (window.console && console.log) {
+			console.log('c%[LEO]', 'color:red', arguments);
+		} else {
+			alert(msg);
+		}
+	}
 });
